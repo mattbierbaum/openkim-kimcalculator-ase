326e6858b7275b81e9b999a841bd1a57ab87655c
Matt Bierbaum <m-bierbaum@jdubs.(none)>
2012-06-12 10:55:46 -0400

    kim_dir to kim_models_dir

--- kimcalculator.py
+++ kimcalculator.py
@@ -415,13 +415,13 @@ def checkIndex(pkim,variablename):
 def listmodels():   
     import os, glob
     try:
-        kimdir = os.environ['KIM_DIR']
+        kimdir = os.environ['KIM_MODELS_DIR']
     except:
-        print "No KIM_DIR set"
+        print "No KIM_MODELS_DIR set"
         return
   
     models = [] 
-    for model in glob.glob(kimdir+'MODELs/*'):
+    for model in glob.glob(kimdir+'/*'):
         models.append(os.path.basename(model))
     return models
 
